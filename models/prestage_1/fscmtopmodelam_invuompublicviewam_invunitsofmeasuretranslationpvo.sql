dbcab10c1181f86973e11f2029685c62dd4fc337
dbtgbqdev <sriguttula93@gmail.com>
2024-07-17 15:08:51 +0530

    Creating a new file for fscmtopmodelam_invuompublicviewam_invunitsofmeasuretranslationpvo

---
+++ models/prestage_1/fscmtopmodelam_invuompublicviewam_invunitsofmeasuretranslationpvo.sql
@@ -0,0 +1,15 @@
+
+{
+    config(
+        materialized='incremental',
+        unique_key='Language,UnitOfMeasureId'
+    )
+}
+
+SELECT * FROM dbt-big-query-project-428712.prestage1.fscmtopmodelam_invuompublicviewam_invunitsofmeasuretranslationpvo
+{% if is_incremental() %}
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records whose timestamp occurred since the last run of this model)
+  -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
+  WHERE LASTUPDATEDATE >= (SELECT COALESCE(MAX(LASTUPDATEDATE), '1900-01-01'::TIMESTAMP) FROM { this })
+{% endif %}
