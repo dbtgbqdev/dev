a88fff01db151855011b0bed09a1cb6c9004d026
dbtgbqdev <sriguttula93@gmail.com>
2024-07-18 11:34:23 +0530

    Creating a new file for fscmtopmodelam_finapinvsetuppmttermsam_paymenttermheadertranslationpvo

---
+++ models/prestage_1/fscmtopmodelam_finapinvsetuppmttermsam_paymenttermheadertranslationpvo.sql
@@ -0,0 +1,12 @@
+{{ config(
+    materialized='incremental',
+    unique_key='Language,TermId,LastUpdateDate'
+) }}
+
+SELECT * FROM osjl-kt.prestage.fscmtopmodelam_finapinvsetuppmttermsam_paymenttermheadertranslationpvo
+{% if is_incremental() %}
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records whose timestamp occurred since the last run of this model)
+  -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
+  WHERE LASTUPDATEDATE >= (SELECT MAX(LASTUPDATEDATE) FROM { this })
+{% endif %}
