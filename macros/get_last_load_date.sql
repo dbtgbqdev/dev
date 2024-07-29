8fe8c5a09c8a0dd76f78b6b2caefdcff3788c1e2
Srinivas Guttula <sriguttula93@gmail.com>
2024-07-29 07:32:14 +0000

    commit

--- macros/get_last_load_date.sql
+++ macros/get_last_load_date.sql
@@ -1,6 +1,6 @@
 {% macro get_last_load_date() %}
 {% set query %}
-   select IFNULL(CAST(max(last_load_date) AS STRING FORMAT 'YYYY-MM-DD'),'1900-01-01') from dbt_duser.dbt_results WHERE table_name = '{{ this.name }}' and status = 'success'
+   select IFNULL(CAST(max(LAST_EXTRACT_DATE) AS STRING FORMAT 'YYYY-MM-DD'),'1900-01-01') from control_tables.OSJL_TASK_CONTROL_G WHERE table_name = '{{ this.name }}' 
 {% endset %}
 {% set results = run_query(query) %}
 {% if execute %}
