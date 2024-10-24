d24fb962577e9bdf7f7ad78989e2701952451538
Srinivas Guttula <sriguttula93@gmail.com>
2024-10-24 07:08:44 +0000

    fgn

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
+    {%- if custom_schema_name is none -%}ryezsthersgy6rtvfsrzt5h
 
         {{ default_schema }}
 
